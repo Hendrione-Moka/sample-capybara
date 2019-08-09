b4feeb14d88444db7471089457ad22b0a42c5381
Hendri Antomy <hendri.antomy@mokapos.com>
2019-08-09 18:25:44 +0700

    update travis.yml;

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,9 +7,15 @@ caps["version"] = "74.0"
 caps["enableVNC"] = true
 caps[Selenium::WebDriver::Chrome::Options::KEY] = {args: %w(--incognito --start-maximized)}
 
+remote_caps = {browser: :remote, url: "http://18.138.40.17:4444/wd/hub", desired_capabilities: caps}
+local_caps = {browser: :chrome, desired_capabilities: caps}
+
+use_remote = false
+
 Capybara.register_driver :chrome do |app|
 
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: "http://18.138.40.17:4444/wd/hub", desired_capabilities: caps)
+  r = use_remote ? remote_caps : local_caps
+  Capybara::Selenium::Driver.new(app, r)
   end
 
 
